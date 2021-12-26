b2d65fddbaebb85f717757835f09dda771d3de18
gerrifox28 <fox.ge@northeastern.edu>
2021-12-26 16:06:46 -0500

    Take request body inputs for filters

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from bs4 import BeautifulSoup
-from flask import Flask, request
+from flask import Flask, json, request
 from selenium import webdriver
 from clean_tickets_list import clean_tickets_list
 
 from convert_url import convert_url
+from find_filtered_matches import find_filtered_matches
 from find_perfect_match import find_perfect_match
 from get_ticket_list import get_ticket_list
 from open_matches_url import open_matches_url
 
-#from search_stubhub import search_stubhub
 
 def create_app():
     app = Flask(__name__)
-    print('print test')
     return app
 
 app = create_app()
@@ -21,28 +20,16 @@ app = create_app()
 def hello():
     return '<h1>Hello, World!</h1>'
 
-@app.route('/search-stubhub/')
-def search():
-    #url='https://www.stubhub.com/olivia-rodrigo-new-york-tickets-4-26-2022/event/105133478/'
-    
-    args = request.args.to_dict()
-    print(args)
-    return 'done'
-    # soup, driver = convert_url(url)
-
-    # tickets = get_ticket_list(soup)
-
-    # tickets = clean_tickets_list(tickets)
-
-    # return find_perfect_match('3rd Mezzanine 4', 'D', 1, 700, driver, tickets)[0]
-   # return result[0]
-
-@app.route('/search')
+@app.route('/search-stubhub')
 def search_url():
     args = request.args.to_dict()
     event = args['event']
     location = args['location']
     date = args['date']
+    filters = json.loads(json.dumps(request.form))
+    all = bool(filters.pop('all'))
+    filters['price'] = int(filters['price'])
+    filters['num_tickets'] = int(filters['num_tickets'])
 
     driver = webdriver.Chrome(executable_path="chromedriver.exe")
     driver.get(f'https://www.stubhub.com/find/s/?q={event}+{location}+{date}')
@@ -51,6 +38,7 @@ def search_url():
 
     event = soup.find('div', class_='EventRedirection')
     if event is None:
+        driver.quit()
         return 'No Events Found'
     link = event.find('a', href=True)
     href = link['href']
@@ -61,12 +49,13 @@ def search_url():
 
     tickets = clean_tickets_list(tickets)
 
-    matches = find_perfect_match('3rd Mezzanine 4', 'D', 1, 700, driver, tickets)
-
-    driver.quit()
+    if len(filters) > 0:
+        matches = find_filtered_matches(tickets, filters, driver, all)
+        url_list = open_matches_url(matches)
+        url_list_to_return = ', '.join([str(elem) for elem in url_list])
+        driver.quit()
+        return url_list_to_return
     
-    url_list = open_matches_url(matches)
+    driver.quit()
+    return 'Add filters to search tickets by'
 
-    url_list_to_return = ', '.join([str(elem) for elem in url_list])
-
-    return url_list_to_return
