5a0faab540a4c5562fb9c5f642a82c97c06a7fbd
gerrifox28 <fox.ge@northeastern.edu>
2021-12-26 21:00:22 -0500

    Delete print statements

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def search_url():
     location = args['location']
     date = args['date']
     filters = json.loads(json.dumps(request.form))
-    print(filters)
 
     driver = webdriver.Chrome(executable_path="chromedriver.exe")
     driver.get(f'https://www.stubhub.com/find/s/?q={event}+{location}+{date}')
@@ -44,7 +43,6 @@ def search_url():
     if len(filters) > 0:
         open_url = False
         all = ast.literal_eval(filters.pop('all'))
-        print(all, 'all')
         if 'price' in filters.keys():
             filters['price'] = int(filters['price'])
         if 'num_tickets' in filters.keys():
@@ -53,7 +51,6 @@ def search_url():
             open_url = ast.literal_eval(filters.pop('open_url'))
         matches, url_list = find_filtered_matches(tickets, filters, driver, all)
         if open_url:
-            print('open url')
             open_matches_url(matches)
         url_list_to_return = ', '.join([str(elem) for elem in url_list])
         driver.close()
